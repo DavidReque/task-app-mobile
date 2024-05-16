265bf7ebceb061f162fc217b2536fc2769e6738b
David Requeno <davidrequeno52@gmail.com>
2024-05-15 21:35:40 -0600

    git

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0UddEwletgmDVjG_nf6Vu7xNael3stLw",
+  authDomain: "task-app-e8033.firebaseapp.com",
+  projectId: "task-app-e8033",
+  storageBucket: "task-app-e8033.appspot.com",
+  messagingSenderId: "91679401894",
+  appId: "1:91679401894:web:da8bfb1619c9e2e1265167",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
